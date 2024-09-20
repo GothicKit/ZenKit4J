078044032f7fe25b0b101196b6206533a302f3fc
Luis Michaelis <me@lmichaelis.de>
2024-09-20 19:32:07 +0200

    feat: add string encoding option when loading the library

--- src/main/java/dev/gothickit/zenkit/capi/ZenKit.java
+++ src/main/java/dev/gothickit/zenkit/capi/ZenKit.java
@@ -10,14 +10,23 @@ public final class ZenKit {
 	public static final Cleaner CLEANER = Cleaner.create();
 	public static ZenKitNative API;
 
-	public static ZenKitNative load(String lib) {
+	public static ZenKitNative load(String encoding, String lib) {
 		var options = new HashMap<String, Object>();
 		options.put(Library.OPTION_TYPE_MAPPER, new ZenKitTypeMapper());
+		options.put(Library.OPTION_STRING_ENCODING, encoding);
 		return Native.load(lib, ZenKitNative.class, options);
 	}
 
-	public static ZenKitNative load() {
-		if (API == null) API = load("zenkitcapi");
+	public static ZenKitNative load(String encoding) {
+		if (API == null) API = load(encoding, "zenkitcapi");
 		return API;
+	}
+
+	public static ZenKitNative load() {
+		return load("Windows-1252");
+	}
+
+	public static void unload() {
+		API = null;
 	}
 }
